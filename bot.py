74635b7563c884ccf493c0caf3105c7fc3a22e1c
Devin Rankin <tun80253@temple.edu>
2023-04-10 13:24:07 -0400

    refactoring bot structure, adding main and bot file, as well as a command package to add future commands

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+import commands
+
+async def send_message(message, user_message):
+    try:
+        response = commands.handle_command(user_message)
+        await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_bot():
+    print(discord.__version__)
+    intents = discord.Intents.default()
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'Logged in as {client.user}.')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f'{username} said {user_message} in ({channel}).')
+
+        if user_message[0] == '&':
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+    
+    client.run(TOKEN)
