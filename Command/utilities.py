f15e9ac1a89386ec31c216fdc5f041430e949fe5
Devin Rankin <tun80253@temple.edu>
2023-04-15 15:24:36 -0400

    more utilities added: help and poll, assignment-related command file created

--- Command/utilities.py
+++ Command/utilities.py
@@ -1,7 +1,25 @@
+import bot
+
 def get_command(message : str) -> str:
     p_message = message.lower()
 
     if p_message == 'help':
-        # do help fn
-        return
+        return help()
     
+@bot.command()
+async def help(ctx):
+    ctx.send("Welcome to the Canvas Helper bot.  Here are the commands you can use: \
+             \n help - prints this message \
+             \n Announcements - prints the announcements for the course \
+             \n Grade - prints your current grade for a course \
+             \n Poll - creates a poll for a course")
+
+async def announcement(message : str) -> str:
+    return ""
+
+@bot.command()
+async def create_poll(ctx, arg):
+    emojis = [':white_check_mark:', ':x:']
+    await ctx.send(arg)
+    for emoji in emojis:
+        await ctx.add_reaction(emoji)
