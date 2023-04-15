df8de1d2487550163a0095bbf49ace3f200ffdac
Devin Rankin <tun80253@temple.edu>
2023-04-15 15:28:27 -0400

    removed old/unused segments

--- bot.py
+++ bot.py
@@ -1,12 +1,4 @@
 import discord
-import commands
-
-async def send_message(message, user_message):
-    try:
-        response = commands.handle_command(user_message)
-        await message.channel.send(response)
-    except Exception as e:
-        print(e)
 
 def run_bot():
     print(discord.__version__)
@@ -27,11 +19,5 @@ def run_bot():
         channel = str(message.channel)
 
         print(f'{username} said {user_message} in ({channel}).')
-
-        if user_message[0] == '&':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
     
     client.run(TOKEN)
