af00f07ab37f1a6204a6181c3e24fe79a69a0a5e
giotata <gitata02@outlook.com>
2023-04-15 21:26:40 -0400

    course() changes course acted on by other commands

--- CanvasInfoBot.py
+++ CanvasInfoBot.py
@@ -3,32 +3,61 @@ import keys
 from discord.ext import commands
 import canvasapi
 import datetime 
+import requests
 
-TOKEN = keys.tokens["discord"]
-CHANNEL_ID = 1075559490289668201
+DISCORD = keys.tokens["discord"]
+CANVAS = keys.tokens['canvas']
+BASEURL = 'https://templeu.instructure.com/'
+canvas_api = canvasapi.Canvas(BASEURL, CANVAS)
+
+current_class = canvas_api.get_courses()[0]
 
 bot = commands.Bot(command_prefix = "&", intents = discord.Intents.all())
 
 @bot.event
 async def on_ready():
     print("Canvas Info Bot up and running!")
-    #channel = bot.get_channel(CHANNEL_ID)
-    #await channel.send("Canvas Info Bot up and running!")
+
+@bot.command()
+async def course(ctx):
+    await ctx.send("Your courses:\n")
+
+    courses = canvas_api.get_courses(enrollment_state='active')
+
+    select = 0
+    for course in courses:
+        name = course.name
+        id = course.id
+        await ctx.send(f"({select}) {name}\n")
+        select += 1
+
+    await ctx.send(f"select a course\n") 
+
+    def check(m):
+        if m.content.isdigit():
+            global pick 
+            pick = int(m.content)
+            return range(0,select).count(pick) > 0
+
+    msg = await bot.wait_for('message', check=check, timeout = 15)
+    print(courses[pick].id)
+    global current_class 
+    current_class = canvas_api.get_course(courses[pick].id)
+    await ctx.send(f'{courses[pick].name}\n')
+    
+@bot.command()
+async def select(ctx):
+    print("Select a course")
 
 @bot.command()
 async def upcoming(ctx):
     none_upcoming = True
-    TOKEN2 = keys.tokens['canvas']
-    BASEURL = 'https://templeu.instructure.com/'
-
-    canvas_api = canvasapi.Canvas(BASEURL, TOKEN2)
 
     user = canvas_api.get_user('self')
-
     print(user.name)
 
-    softwareDesign = canvas_api.get_course(123654) 
-    assignments = softwareDesign.get_assignments()
+    #softwareDesign = canvas_api.get_course(123654) 
+    assignments = current_class.get_assignments()
 
     for assignment in assignments:
         due_date = str(assignment.due_at)
@@ -44,8 +73,7 @@ async def upcoming(ctx):
                 print(f"{assignment} is due on {readable_date} at {readable_time}\n")
                 await ctx.send(f"{assignment}\n **due on {readable_date} at {readable_time}**\n\n")
     
-    print(none_upcoming)
     if(none_upcoming):
-        await ctx.send("You have no upcoming assignments in this class!")
+        await ctx.send(f"You have no upcoming assignments in {current_class.name}!")
 
-bot.run(TOKEN)
+bot.run(DISCORD)
