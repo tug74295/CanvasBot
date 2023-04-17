f85dc970ce4dbde92b257f457a776e74acaf4bc9
giotata <gitata02@outlook.com>
2023-04-17 13:26:29 -0400

    made nicer output, renamed main

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import keys
 from discord.ext import commands
 import canvasapi
 import datetime 
-import requests
+import pytz
 
 DISCORD = keys.tokens["discord"]
 CANVAS = keys.tokens['canvas']
 BASEURL = 'https://templeu.instructure.com/'
 canvas_api = canvasapi.Canvas(BASEURL, CANVAS)
 
-current_class = canvas_api.get_courses()[0]
+current_class = canvas_api.get_courses()[5]
 
 bot = commands.Bot(command_prefix = "&", intents = discord.Intents.all())
 
@@ -20,7 +20,7 @@ async def on_ready():
 
 @bot.command()
 async def course(ctx):
-    await ctx.send("Your courses:\n")
+    await ctx.send("Here are your courses:\n")
 
     courses = canvas_api.get_courses(enrollment_state='active')
 
@@ -31,7 +31,7 @@ async def course(ctx):
         await ctx.send(f"({select}) {name}\n")
         select += 1
 
-    await ctx.send(f"select a course\n") 
+    await ctx.send(f"Enter a number to select the corresponding course\n") 
 
     def check(m):
         if m.content.isdigit():
@@ -43,11 +43,7 @@ async def course(ctx):
     print(courses[pick].id)
     global current_class 
     current_class = canvas_api.get_course(courses[pick].id)
-    await ctx.send(f'{courses[pick].name}\n')
-    
-@bot.command()
-async def select(ctx):
-    print("Select a course")
+    await ctx.send(f'Current course: **{courses[pick].name}**\n')
 
 @bot.command()
 async def upcoming(ctx):
@@ -62,16 +58,15 @@ async def upcoming(ctx):
     for assignment in assignments:
         due_date = str(assignment.due_at)
 
-        readable = due_date
         if(due_date != "None"):
             t1 = datetime.datetime(int(due_date[0:4]), int(due_date[5:7]), int(due_date[8:10]))
             t2 = datetime.datetime.now()
             if(t1>t2):
                 none_upcoming = False
-                readable_date = f"{due_date[5:10]}-{due_date[0:4]}"
                 readable_time = f"{due_date[11:16]} UTC"
+                readable_date = t1.strftime("%A, %B %d")
                 print(f"{assignment} is due on {readable_date} at {readable_time}\n")
-                await ctx.send(f"{assignment}\n **due on {readable_date} at {readable_time}**\n\n")
+                await ctx.send(f"```diff\n- {assignment.name} -\ndue on {readable_date} at {readable_time}```\n\n")
     
     if(none_upcoming):
         await ctx.send(f"You have no upcoming assignments in {current_class.name}!")
