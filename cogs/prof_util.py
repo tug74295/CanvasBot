8f56cd574a53cdcf0cacdfe1792c48ed44fe0894
Devin Rankin <tun80253@temple.edu>
2023-04-24 12:31:40 -0400

    poll embed working, need to add reactions

--- cogs/prof_util.py
+++ cogs/prof_util.py
@@ -2,18 +2,46 @@ import nextcord
 from nextcord.ext import commands
 from nextcord import Interaction
 from nextcord.ext.commands import has_permissions, MissingPermissions
+from nextcord import SlashOption
+from nextcord import Embed
+from datetime import datetime
 
 class prof_util(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    # Our test server id. Change in the future.
+    server_id = 1075559489631170590
+
     @nextcord.slash_command(name='announce', description='Make an announcement on Canvas.')
     async def announcement(self, interaction : Interaction, title : str, content : str):
         pass
 
-    @nextcord.slash_command(name='poll', description='Create a poll.')
-    async def create_poll(self, interaction : Interaction, title : str, content : str):
-        pass
+    @nextcord.slash_command(name='poll', description='Create a poll.', guild_ids=[server_id])
+    async def create_poll(self, 
+                          interaction : Interaction,
+                          question : str = SlashOption(name='question'),
+                          options : str = SlashOption(name='options')):
+        numbers = ("1️⃣", "2⃣", "3⃣", "4⃣", "5⃣",
+		   "6⃣", "7⃣", "8⃣", "9⃣", "🔟")
 
+        options_list = options.split()
+            
+        if len(options) > 10:
+            await interaction.response.send_message("You can only supply a maximum of 10 options.")
+            return
+
+        embed = Embed(title='Poll',
+                      description=question,
+                      color=interaction.user.color,
+                      timestamp=datetime.utcnow()
+                      )
+        fields=[("Options", "\n".join([f'{numbers[i]} {option}' for i, option in enumerate(options_list)]), False),
+                ("Instructions", "React to cast a vote.", False)]
+
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+
+        await interaction.response.send_message(embed=embed)
 def setup(client):
     client.add_cog(prof_util(client))
--- cogs/stud_util.py
+++ cogs/stud_util.py
@@ -8,19 +8,23 @@ class stud_util(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    # Our test server id. Change in the future.
+    server_id = 1075559489631170590
+
     @nextcord.slash_command(name='upcoming', description='List the upcoming assignments.')
     async def get_upcoming(self, interaction : Interaction, course : str):
         pass
 
     @nextcord.slash_command(name='grades', description='View grade for a specific class.')
-    async def view_grade(self, interaction : Interaction, course : str):
+    async def view_grade(self, interaction : Interaction):
         pass
 
     @nextcord.slash_command(name='due', description='Get due date for a specific assignment.')
     async def get_due_date(self, interaction : Interaction, assignment : str):
         pass
+
     @nextcord.slash_command(name='courses', description='List enrolled courses.')
-    async def get_courses(self, interaction : Interaction):
+    async def get_courses(self, interaction : Interaction) -> canvasapi.course.Course:
         await interaction.response.send_message("Here are your courses:\n")
 
         courses = canvasapi.get_courses(enrollment_state='active')
@@ -32,19 +36,21 @@ class stud_util(commands.Cog):
             await interaction.response.send_message(f"({select}) {name}\n")
             select += 1
 
-        await interaction.response.send_message(f"Enter a number to select the corresponding course\n") 
+        await interaction.response.send_message(f"Enter a number to select the corresponding course\n")
 
-        def check(m):
-            if m.content.isdigit():
+        def check(message : nextcord.message):
+            if message.content.isdigit():
                 global pick 
-                pick = int(m.content)
+                pick = int(message.content)
                 return range(0,select).count(pick) > 0
-
-        msg = await self.client.wait_for('message', check=check, timeout = 15)
+            
+        await self.client.wait_for('message', check=check, timeout = 15)
         print(courses[pick].id)
         global current_class 
         current_class = canvasapi.get_course(courses[pick].id)
         await interaction.response.send_message(f'Current course: **{courses[pick].name}**\n')
+        self.curr_course = courses[pick]
+
 
 def setup(client):
     client.add_cog(stud_util(client))
