5e0df15b43ad3e70d6b3822a171f703968c9d860
Devin Rankin <tun80253@temple.edu>
2023-04-25 02:30:40 -0400

    implemented oauth for courses command

--- cogs/stud_util.py
+++ cogs/stud_util.py
@@ -3,6 +3,7 @@ from nextcord.ext import commands
 from nextcord import Interaction
 from nextcord.ext.commands import has_permissions, MissingPermissions
 import canvasapi
+import json
 
 class stud_util(commands.Cog):
     def __init__(self, client):
@@ -11,32 +12,41 @@ class stud_util(commands.Cog):
     # Our test server id. Change in the future.
     server_id = 1075559489631170590
 
-    @nextcord.slash_command(name='upcoming', description='List the upcoming assignments.')
-    async def get_upcoming(self, interaction : Interaction, course : str):
-        pass
-
-    @nextcord.slash_command(name='grades', description='View grade for a specific class.')
-    async def view_grade(self, interaction : Interaction):
-        pass
-
-    @nextcord.slash_command(name='due', description='Get due date for a specific assignment.')
-    async def get_due_date(self, interaction : Interaction, assignment : str):
-        pass
+    def get_user_canvas(self, member : nextcord.User | nextcord.Member,
+                        filename = 'users.json') -> str:
+        with open(filename, 'r+') as file:
+            file_data = json.load(file)
+            for user in file_data['users']:
+                print(user['snowflake'])
+                if user['snowflake'] == member.id:
+                    return user['apikey']
+            return "Please login using the /login command!"
 
     @nextcord.slash_command(name='courses', description='List enrolled courses.')
-    async def get_courses(self, interaction : Interaction) -> canvasapi.course.Course:
+    async def get_courses(self, interaction : Interaction):
+        
+        API_URL = 'https://templeu.instructure.com/'
+        api_key = self.get_user_canvas(member=interaction.user)
+
+        if api_key == 'Please login using the /login command!':
+            await interaction.response.send_message(api_key)
+            return
+        
+        user = canvasapi.Canvas(API_URL, api_key)
+        courses = user.get_courses(enrollment_state='active')
+
         await interaction.response.send_message("Here are your courses:\n")
 
-        courses = canvasapi.get_courses(enrollment_state='active')
-
         select = 0
+        output = ""
         for course in courses:
             name = course.name
             id = course.id
-            await interaction.response.send_message(f"({select}) {name}\n")
+            output += f"({select}) {name}\n"
             select += 1
 
-        await interaction.response.send_message(f"Enter a number to select the corresponding course\n")
+        output += "+ Enter a number to select the corresponding course +\n"
+        await interaction.followup.send(f"```diff\n{output}```") 
 
         def check(message : nextcord.message):
             if message.content.isdigit():
@@ -47,9 +57,9 @@ class stud_util(commands.Cog):
         await self.client.wait_for('message', check=check, timeout = 15)
         print(courses[pick].id)
         global current_class 
-        current_class = canvasapi.get_course(courses[pick].id)
-        await interaction.response.send_message(f'Current course: **{courses[pick].name}**\n')
-        self.curr_course = courses[pick]
+
+        current_class = user.get_course(courses[pick].id)
+        await interaction.followup.send(f'Current course: **{courses[pick].name}**\n')
 
 
 def setup(client):
